db7f611538d9e22597553b9ca2433ea162e52f63
Tychio <code@tychio.net>
2020-12-10 15:33:29 +0100

    add demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <JWT />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JWT from './components/JWT.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JWT
   }
 }
 </script>
---
+++ src/components/JWT.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Truck JWT login Button Demo</h1>
+    <p>Clicking button to login with the JWT token:</p>
+    <p><code>{{jwt}}</code></p>
+    <a :href="loginLink" class="btn">Login</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JWT',
+  data () {
+    const search = new URLSearchParams(window.location.search)
+    return {
+      jwt: search.get('jwt')
+    }
+  },
+  computed: {
+    loginLink() {
+      return 'http://44.227.72.76/admin/jwt.php?token=' + this.jwt
+    }
+  }
+}
+</script>
+<style scoped>
+a.btn {
+  display: inline-block;
+  font-size: 1.2rem;
+  color: #fff;
+  margin-top: 10px;
+  cursor: pointer;
+  background-color: #3eaf7c;
+  padding: .8rem 1.6rem;
+  border-radius: 4px;
+  transition: background-color .1s ease;
+  box-sizing: border-box;
+  text-decoration: none;
+  border-bottom: 1px solid #389d70;
+}
+a.btn:hover {
+  background-color: #4abf8a;
+}
+code {
+  background: #f1f1f1;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+}
+</style>
